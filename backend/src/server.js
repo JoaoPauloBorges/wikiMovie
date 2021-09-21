9dacf3ad13f818f540db728f8c7be6179b1c6483
João Paulo Borges <joaopaulo.borges.gm@hotmail.com>
2021-09-21 12:49:54 -0300

    allow cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,13 @@ const filesRouter = require('./api/routes/files.router')
 async function startServer() {
     const app = express();
     app.use(express.json());
-    app.use(cors());
+    // app.use(cors());
+
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
 
     // routes
     app.use('/api/movies',moviesRouter);    
